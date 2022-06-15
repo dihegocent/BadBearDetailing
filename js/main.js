20a538a1ac2933e8be022d523a6243e49bdf690a
dihegocent <94179594+dihegocent@users.noreply.github.com>
2022-06-15 12:44:53 -0400

    script
    ver scrpit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ const app = document.getElementById('typewriter');
 const typewriter = new Typewriter(app, {loop:true,delay:75});
 
 
-typewriter.typeString('La ciudad de la escoba'):
+typewriter.typeString("La ciudad de la escoba");
 typewriter.pauseFor(200);
 typewriter.start();
