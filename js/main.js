c9ad65bfb967ecae8acb6cd6484146936f9858c4
dihegocent <94179594+dihegocent@users.noreply.github.com>
2022-06-15 12:23:11 -0400

    aesthetic details
    Waves to down

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const app = document.getElementById('typewriter');
+
+const typewriter = new Typewriter(app, {loop:true,delay:75})
+
+
+typewriter
+.typeString('La ciudad de la escoba')
+.pauseFor(200)
+.star()
