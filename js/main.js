86c0e80e221aeba73fcd4f9733308bab31c279d9
dihegocent <94179594+dihegocent@users.noreply.github.com>
2022-06-17 11:05:51 -0400

    arreglo del padding
    arreglado el mapa y padding en donde se encuentra el typewrite

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const app = document.getElementById('typewriter');
 const typewriter = new Typewriter(app, {loop:true,delay:75});
 
 
-typewriter.typeString("La ciudad de la escoba");
-typewriter.pauseFor(200);
-typewriter.start();
+typewriter
+    .typeString('La ciudad de la escoba')
+    .pauseFor(200)
+    .start();
